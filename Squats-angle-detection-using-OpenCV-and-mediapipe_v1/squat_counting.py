05168e120fb5fd5e68e8c1756821bd90a236ee7e
AndroBuddy <anirudhchimpidi8@gmail.com>
2022-12-13 17:42:01 +0530

    Working ~ Rounak
    Signed-off-by: AndroBuddy <anirudhchimpidi8@gmail.com>

--- Squats-angle-detection-using-OpenCV-and-mediapipe_v1/squat_counting.py
+++ Squats-angle-detection-using-OpenCV-and-mediapipe_v1/squat_counting.py
@@ -4,7 +4,7 @@ import mediapipe as mp
 import numpy as np
 
 from flask_cors import CORS
-from flask import Flask, jsonify
+from flask import Flask, jsonify, Response
 
 # configuration
 DEBUG = True
@@ -14,6 +14,8 @@ app = Flask(__name__, template_folder='./temp')
 
 # enable CORS
 CORS(app, resources={r'/*': {'origins': '*'}})
+
+cap = cv2.VideoCapture(0)
 
 
 headers = {"Content-Type": "application/json;charset=UTF-8"}
@@ -52,133 +54,149 @@ angle_min = []
 angle_min_hip = []
 
 # webcam input
-cap = cv2.VideoCapture(0)
 
-
-@app.route('/', methods=['GET'])
-def capture():
+def gen_frames():
     # Curl counter variables
     counter = 0
+
+
     stage = None
 
     with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
         while cap.isOpened():
             ret, frame = cap.read()
+            if ret:
+                # Recolor image to RGB
+                image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                image = cv2.flip(image, 1)
+                image.flags.writeable = False
 
-            # Recolor image to RGB
-            image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            image = cv2.flip(image, 1)
-            image.flags.writeable = False
+                # Make detection
+                results = pose.process(image)
 
-            # Make detection
-            results = pose.process(image)
+                # Recolor back to BGR
+                image.flags.writeable = True
+                image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
-            # Recolor back to BGR
-            image.flags.writeable = True
-            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+                # Extract landmarks
+                try:
+                    landmarks = results.pose_landmarks.landmark
 
-            # Extract landmarks
+                    # Get coordinates
+                    shoulder = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x,
+                                landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y]
+                    elbow = [landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].x,
+                            landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].y]
+                    wrist = [landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].x,
+                            landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y]
+
+                    # Get coordinates
+                    hip = [landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].x,
+                        landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].y]
+                    knee = [landmarks[mp_pose.PoseLandmark.LEFT_KNEE.value].x,
+                            landmarks[mp_pose.PoseLandmark.LEFT_KNEE.value].y]
+                    ankle = [landmarks[mp_pose.PoseLandmark.LEFT_ANKLE.value].x,
+                            landmarks[mp_pose.PoseLandmark.LEFT_ANKLE.value].y]
+
+                    # Calculate angle
+                    angle = calculate_angle(shoulder, elbow, wrist)
+
+                    angle_knee = calculate_angle(
+                        hip, knee, ankle)  # Knee joint angle
+
+                    angle_hip = calculate_angle(shoulder, hip, knee)
+                    hip_angle = 180-angle_hip
+                    knee_angle = 180-angle_knee
+
+                    # Visualize angle
+                    """cv2.putText(image, str(angle), 
+                                tuple(np.multiply(elbow, [640, 480]).astype(int)), 
+                                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA
+                                        )"""
+
+                    cv2.putText(image, str(angle_knee),
+                                tuple(np.multiply(knee, [640, 480]).astype(int)),
+                                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (79,
+                                                                121, 66), 2, cv2.LINE_AA
+                                )
+
+                    """cv2.putText(image, str(angle_hip), 
+                                tuple(np.multiply(hip, [640, 480]).astype(int)), 
+                                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA
+                                        )"""
+
+                    # Curl counter logic
+                    if angle_knee > 169:
+                        stage = "UP"
+                        # response,content=http.request(url_switch_off,"POST",headers=headers)
+                        flag = True
+
+                    if angle_knee <= 90 and stage == 'UP':
+                        stage = "DOWN"
+                        counter += 1
+                        # response,content=http.request(url_switch_on,"POST",headers=headers)
+                        print(counter)  # Counter for squat
+                except:
+                    pass
+
+                # Render squat counter
+                # Setup status box
+                cv2.rectangle(image, (0, 0), (225, 73), (245, 117, 16), -1)
+
+                # Rep data
+                cv2.putText(image, 'REPS', (15, 12),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
+                cv2.putText(image, str(counter),
+                            (10, 60),
+                            cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255), 2, cv2.LINE_AA)
+
+                # Stage data
+                cv2.putText(image, 'STAGE', (65, 12),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
+                cv2.putText(image, stage,
+                            (60, 60),
+                            cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255), 2, cv2.LINE_AA)
+
+                # Render detections
+                mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
+                                        mp_drawing.DrawingSpec(
+                                            color=(245, 117, 66), thickness=2, circle_radius=2),
+                                        mp_drawing.DrawingSpec(
+                                            color=(245, 66, 230), thickness=2, circle_radius=2)
+                                        )
+
+                # out.write(image)
+                # cv2.imshow('Squatty', image)
+
+                # if cv2.waitKey(10) & 0xFF == ord('q'):
+                #     break
+
+                # return jsonify({
+                #     'status': 'success',
+                #     'count': str(counter),
+                #     'rep': stage
+                # })
             try:
-                landmarks = results.pose_landmarks.landmark
-
-                # Get coordinates
-                shoulder = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x,
-                            landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y]
-                elbow = [landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].x,
-                         landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].y]
-                wrist = [landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].x,
-                         landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y]
-
-                # Get coordinates
-                hip = [landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].x,
-                       landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].y]
-                knee = [landmarks[mp_pose.PoseLandmark.LEFT_KNEE.value].x,
-                        landmarks[mp_pose.PoseLandmark.LEFT_KNEE.value].y]
-                ankle = [landmarks[mp_pose.PoseLandmark.LEFT_ANKLE.value].x,
-                         landmarks[mp_pose.PoseLandmark.LEFT_ANKLE.value].y]
-
-                # Calculate angle
-                angle = calculate_angle(shoulder, elbow, wrist)
-
-                angle_knee = calculate_angle(
-                    hip, knee, ankle)  # Knee joint angle
-
-                angle_hip = calculate_angle(shoulder, hip, knee)
-                hip_angle = 180-angle_hip
-                knee_angle = 180-angle_knee
-
-                # Visualize angle
-                """cv2.putText(image, str(angle), 
-                            tuple(np.multiply(elbow, [640, 480]).astype(int)), 
-                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA
-                                    )"""
-
-                cv2.putText(image, str(angle_knee),
-                            tuple(np.multiply(knee, [640, 480]).astype(int)),
-                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (79,
-                                                            121, 66), 2, cv2.LINE_AA
-                            )
-
-                """cv2.putText(image, str(angle_hip), 
-                            tuple(np.multiply(hip, [640, 480]).astype(int)), 
-                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA
-                                    )"""
-
-                # Curl counter logic
-                if angle_knee > 169:
-                    stage = "UP"
-                    # response,content=http.request(url_switch_off,"POST",headers=headers)
-                    flag = True
-
-                if angle_knee <= 90 and stage == 'UP':
-                    stage = "DOWN"
-                    counter += 1
-                    # response,content=http.request(url_switch_on,"POST",headers=headers)
-                    print(counter)  # Counter for squat
-            except:
+                frame = image
+                ret, buffer = cv2.imencode('.jpg', frame)
+                frame = buffer.tobytes()
+                yield (b'--frame\r\n'
+                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+            except Exception as e:
+                    pass
+                    
+        else:
                 pass
 
-            # Render squat counter
-            # Setup status box
-            cv2.rectangle(image, (0, 0), (225, 73), (245, 117, 16), -1)
-
-            # Rep data
-            cv2.putText(image, 'REPS', (15, 12),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
-            cv2.putText(image, str(counter),
-                        (10, 60),
-                        cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255), 2, cv2.LINE_AA)
-
-            # Stage data
-            cv2.putText(image, 'STAGE', (65, 12),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
-            cv2.putText(image, stage,
-                        (60, 60),
-                        cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255), 2, cv2.LINE_AA)
-
-            # Render detections
-            mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
-                                      mp_drawing.DrawingSpec(
-                                          color=(245, 117, 66), thickness=2, circle_radius=2),
-                                      mp_drawing.DrawingSpec(
-                                          color=(245, 66, 230), thickness=2, circle_radius=2)
-                                      )
-
-            # out.write(image)
-            cv2.imshow('Squatty', image)
-
-            if cv2.waitKey(10) & 0xFF == ord('q'):
-                break
-
-            return jsonify({
-                'status': 'success',
-                'count': str(counter),
-                'rep': stage
-            })
 
 # @app.route('/')
 # def index():
 #     return render_template('index.html')
+
+
+@app.route('/video_feed', methods=['GET'])
+def video_feed():
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
 if __name__ == '__main__':
